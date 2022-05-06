4bf0098b1ca257289779ee3ba4b0453f65f2b7e7
Lima-49 <vitorlima.soares@hotmail.com>
2022-05-05 21:11:17 -0300

    Commit inicial

---
+++ projeto-final/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './Componentes/Home/Header.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto-final/src/Componentes/Home/Header.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function Header(){
+
+    return(
+        <div className="headerH">
+            <div className="tituloH">Marmitas Express</div>
+        </div>
+    );
+};
