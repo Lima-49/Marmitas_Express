51900facddf6e8faaf1be481e7470c420494f384
Eduardo Candioto Fidelis <educafi41@gmail.com>
2022-05-06 22:14:37 -0300

    Adicion navbar

--- projeto-final/src/App.js
+++ projeto-final/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './components/Home/Header.js'
+import Navbar from './components/Navbar/Navbar';
+
+import { BrowserRouter as Router,
+         Routes,
+         Route,
+         Link } from 'react-router-dom'
+import Home from './components/Home/Home';
+import Register from './components/Register/Register';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+      <Router>
+        <div className="App">
+          <Header/>
+          <Navbar />
+      
+          <Routes>
+            <Route path='/' element={Home()} />
+            <Route path='/cardapio' element={Menu()} />
+            <Route path='/cadastro' element={Register()} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
---
+++ projeto-final/src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to='/home'> Início </Link></li>
+                <li><Link to='/cardapio'> Cardapio </Link></li>
+                <li><Link to='/cadastro'> Cadastro </Link></li>
+            </ul>
+        </nav>
+    )
+}
